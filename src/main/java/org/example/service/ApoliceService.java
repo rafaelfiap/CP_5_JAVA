dbba6d8e748434f2296a7b00a266ec347a169a6d
Rafael <rm557837@fiap.com.br>
2024-10-14 23:32:10 -0300

    teste

--- src/main/java/org/example/service/ApoliceService.java
+++ src/main/java/org/example/service/ApoliceService.java
@@ -12,17 +12,7 @@ import java.time.LocalDate;
  */
 public class ApoliceService {
 
-    /**
-     * Calcula o valor da apólice com base no veículo e no cliente.
-     *
-     * @param apolice A apólice para a qual o valor será calculado.
-     * @return O valor calculado da apólice.
-     */
-    public double calcularValor(Apolice apolice) {
-        double valorBase = apolice.getVeiculo().calcularValorSeguro();  // Calcula o valor base do seguro do veículo
-        double desconto = apolice.getCliente().calcularDesconto();      // Aplica um possível desconto ao cliente
-        return valorBase * (1 - desconto);                              // Retorna o valor final com desconto aplicado
-    }
+
 
 
 
